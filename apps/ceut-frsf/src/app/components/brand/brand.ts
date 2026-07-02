9b7f70fdf6163dc2caa4ae9016ada5eee251bdea
juanblas09 <jbtschopp@gmail.com>
2026-07-01 22:10:24 -0300

    [#1] - Render CEUT logo in Brand component with compact icon for collapsed sidebar

--- apps/ceut-frsf/src/app/components/brand/brand.ts
+++ apps/ceut-frsf/src/app/components/brand/brand.ts
@@ -1,31 +1,28 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 import { RouterLink } from '@angular/router'
-import { NgIcon, provideIcons } from '@ng-icons/core'
-import { featherRefreshCw } from '@ng-icons/feather-icons'
 import { Button } from '@resetshop/ui/button/button'
 
 @Component({
 	selector: 'app-brand',
-	imports: [Button, NgIcon, RouterLink],
+	imports: [Button, RouterLink],
 	host: { class: 'flex items-center p-2' },
 	template: `
-		<!--	TODO: Replace with the navigation to your home page / initial page -->
 		<a
-			[routerLink]="['..', 'dashboard']"
+			[routerLink]="['/dashboard']"
 			[fullWidth]="true"
 			appButton
 			variant="default"
 			size="sm"
 			class="gap-2 font-semibold"
 		>
-			<ng-icon name="featherRefreshCw" data-icon="start" />
-			@if (!collapsed()) {
-				<span class="min-w-0 truncate">Reset Starter Repo</span>
+			@if (collapsed()) {
+				<img src="logo/ceut-icon.png" width="32" height="32" alt="CEUT FRSF" class="size-8 shrink-0" />
+			} @else {
+				<img src="logo/ceut-logo.svg" alt="CEUT FRSF" class="h-8 w-auto" />
 			}
 		</a>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	viewProviders: [provideIcons({ featherRefreshCw })],
 })
 export class Brand {
 	public readonly collapsed = input(false)
